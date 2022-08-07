39b767aa0ff2f417cd43eb2902a65f8fc49299da
mohdzaid <mohdzaid@gmail.com>
2022-08-07 16:28:52 +0530

    inital commit

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 mongoose.connect(
-  process.env.MONGO_DB_URI.toString(),
+  process.env.MONGO_DB_URI,
   {
     useNewUrlParser: true,
     useCreateIndex: true,
